01ae42624003f16d4b39c9d64b357ace0ed2b61f
filipkonopacki <filip.konopacki96@gmail.com>
2020-06-11 19:22:14 +0200

    Initial commit

---
+++ covid19/src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
+import Home from './Home';
+import About from './About';
+
+class App extends Component {
+  render() {
+    return (
+        <Router>
+            <div className="container">
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                </ul>
+
+                <Route exact path="/" component={Home} />
+                <Route path="/about" component={About} />
+            </div>
+        </Router>
+    );
+  }
+}
+
+export default App;
